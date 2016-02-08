ac51df9669857bc29997a8375407dfc1594593f7
jbillote <jbillote@gmail.com>
2016-02-08 15:51:23 -0800

    Added background music and placeholder for footsteps

---
+++ CurrentAnimation.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class CurrentAnimation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum CurrentAnimation  
+{
+    IDLE_LEFT,
+    IDLE_RIGHT,
+    WALKING_LEFT,
+    WALKING_RIGHT;
+}
